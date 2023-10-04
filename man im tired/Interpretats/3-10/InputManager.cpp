cbf23a2c24685d0c484b272e2c34fac35ca7e3cd
JAN PLANES MAZCUÑAN <jan.planes@enti.cat>
2023-10-04 17:52:16 +0200

    CODIGO FINIQUITADO la solucion es la interpretats.sln

--- man im tired/Interpretats/3-10/InputManager.cpp
+++ man im tired/Interpretats/3-10/InputManager.cpp
@@ -35,6 +35,15 @@ void InputManager::ReadLoop()
 		int keyCode = ConsoleControl::WaithForReadNextKey();
 		//^Search in KeyBindings Map if pressed keycode exists
 		//if it does, call all lamda functions (functions with no name)
+		_listenersMapMutex->lock();
+		KeyBindingListsMap::iterator pair = _listenersMap->find(keyCode);
+		if (pair != _listenersMap->end()) {
+			std::list<KeyBinding*>* keyBindings = pair->second;
+			for (KeyBinding* keyBinding : *keyBindings) {
+				keyBinding->onKeyPress(keyCode);
+			}
+		}
+		_listenersMapMutex->unlock();
 		_isStartedMutex->lock();
 		isStarted = _isStarted;
 		_isStartedMutex->unlock();
@@ -45,9 +54,62 @@ void InputManager::StopListener()
 	_isStartedMutex->lock();
 	_isStarted = false;
 	_isStartedMutex->unlock();
-}
-//lock y unlock deben estar separados lo minimo necesario 
+	//lock y unlock deben estar separados lo minimo necesario 
 // para que no se pare el codigo del juego MUCHO RATO
+}
+unsigned int InputManager::AddListener(int keyCode, KeyBinding::OnKeyPress onKeyPress)
+{
+	KeyBinding* binding = new KeyBinding(keyCode, onKeyPress); 
+	_listenersMapMutex->lock();
+
+	KeyBindingListsMap::iterator pair = _listenersMap->find(keyCode);
+	std::list<KeyBinding*>* keyBindings = nullptr;
+	if (pair == _listenersMap->end()) {
+		_listenersMap->emplace(keyCode, keyBindings);
+	}
+	else {
+		keyBindings = pair->second;
+	}
+	keyBindings->push_back(binding);
+
+	_listenersMapMutex->unlock();
+	return binding->GetSubscriptionId();
+}
+void InputManager::RemoveListener(unsigned int subscriptionId)
+{
+	_listenersMapMutex->lock();
+	for (std::pair<int, std::list<KeyBinding*>*> pair : *_listenersMap) {
+		std::list<KeyBinding*>* keyBindings = pair.second;
+		for (KeyBinding* binding : *keyBindings) {
+			if (binding->GetSubscriptionId() == subscriptionId) {
+				keyBindings->remove(binding);
+			}
+		}
+	}
+	_listenersMapMutex->unlock();
+}
 
 
+InputManager::KeyBinding::KeyBinding(int keyCode, OnKeyPress onKeyPress)
+{
+	static std::mutex currentIdMutex = std::mutex();
+	currentIdMutex.lock();
+	static unsigned int currentId = 0;
+	//^gracias a static currentId sera asignado a 0 la primera vez, 
+	// la siguiente vez que llame al constructor currentId 
+	// no se le asignara el valor 0 sino el que le quedo la ultima vez que se la llamo
+	subscriptionId = currentId;
+	currentId++;
+	currentIdMutex.unlock();
+	this->keyCode = keyCode;
+	this->onKeyPress = onKeyPress;
+}
 
+InputManager::KeyBinding::~KeyBinding()
+{
+}
+
+unsigned int InputManager::KeyBinding::GetSubscriptionId()
+{
+	return subscriptionId;
+}
--- man im tired/Interpretats/3-10/InputManager.h
+++ man im tired/Interpretats/3-10/InputManager.h
@@ -1,18 +1,45 @@
 #pragma once
 #include <mutex>
+#include <functional>
+#include <map>
+#include <list>
 class InputManager
 {
+public:
+	class KeyBinding {
+	public:
+		typedef std::function<void(int keyCode)> OnKeyPress;
+		//^funcion lambda, algo que actua como funcion sin nombre, 
+		// entre <> el tipo de funcion y entre () los parametros
+		//^con typedef la funcion lamda de este tipo se llamara OnKeyPress 
+		// y escribiendo OnKeyPress es como si escribieramos lo de std::function....
+
+		
+		int keyCode;
+		OnKeyPress onKeyPress;
+		
+		KeyBinding(int keyCode, std::function<void(int keyCode)> onKeyPress);
+		~KeyBinding();
+		unsigned int GetSubscriptionId();
+	private:
+		unsigned int subscriptionId = 0;
+	};
+	
+private:
+	std::mutex* _isStartedMutex = new std::mutex();
+	typedef std::map<int, std::list<KeyBinding*>*> KeyBindingListsMap;
+	KeyBindingListsMap* _listenersMap = new KeyBindingListsMap();
+	std::mutex* _listenersMapMutex = new std::mutex();
+	bool _isStarted = false;
+	std::thread* _listenerThread;
+	void ReadLoop();
 public:
 	InputManager();
 	~InputManager();
 	void StartListener();
 	void StopListener();
-	unsigned int AddListener(int keyCode);
-	void RemoveList(unsigned int listenerId);
-private:
-	std::mutex* _isStartedMutex = new std::mutex();
-	bool _isStarted = false;
-	std::thread* _listenerThread;
-	void ReadLoop();
+	unsigned int AddListener(int keyCode, KeyBinding::OnKeyPress);
+	void RemoveListener(unsigned int subscriptionId);
+
 };
 
